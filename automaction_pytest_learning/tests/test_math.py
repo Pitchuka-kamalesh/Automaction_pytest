9586826c0d835f07a42e667e593f2d95b2da9698
Pitchuka-kamalesh <kamalesh.4025@gmail.com>
2023-12-05 18:52:26 +0530

    change the failed case to pass case

--- automaction_pytest_learning/tests/test_math.py
+++ automaction_pytest_learning/tests/test_math.py
@@ -2,8 +2,16 @@ def test_one_plus_one():
     assert 1 + 1 == 2
 
 
+# def test_one_plus_two(): failed test case to check the report 
+#     a = 1
+#     b = 2
+#     c = 0
+#     assert a + b  == c
+
+
 def test_one_plus_two():
     a = 1
     b = 2
-    c = 0
-    assert a + b  == c
+    c = 3
+    assert a + b == c
+
